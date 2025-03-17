e334b20676cdfe5dc39b8a50a84e1c73cfa4f89a
Giorgio Ripani <72026038+gripani@users.noreply.github.com>
2025-03-17 10:39:40 +0100

    handling of session reloading

--- ultra_framework/mixins/session_mixin.py
+++ ultra_framework/mixins/session_mixin.py
@@ -5,3 +5,7 @@ class SessionMixin:
 
     def __init__(self, session: Session):
         self.session = session
+
+    def update_session(new_session: Session):
+        self.session = new_session
+        
