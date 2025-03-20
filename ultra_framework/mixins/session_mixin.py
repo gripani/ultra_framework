46943e1705d1c0436b7948e8eff1600fcb7200c4
Giorgio Ripani <72026038+gripani@users.noreply.github.com>
2025-03-20 17:23:58 +0100

    Update session_mixin.py

--- ultra_framework/mixins/session_mixin.py
+++ ultra_framework/mixins/session_mixin.py
@@ -6,6 +6,4 @@ class SessionMixin:
     def __init__(self, session: Session):
         self.session = session
 
-    def update_session(new_session: Session):
-        self.session = new_session
         
